f4545d6b25f47b5c1d165df99c0106331562df5e
xr0m3oz <xr0m3oz@gmail.com>
2018-12-22 00:01:28 +0200

    add comment

--- src/UploadFile.php
+++ src/UploadFile.php
@@ -3,53 +3,85 @@
 namespace xr0m3oz\urlparser;
 
 
+/**
+ * Class UploadFile
+ * @package xr0m3oz\urlparser
+ */
 class UploadFile
 {
     protected $url;
-    protected $format = ['image/jpeg','image/png','image/gif'];
+    protected $format = ['image/jpeg', 'image/png', 'image/gif'];
     protected $path;
+    protected $imageType;
 
-    public function __construct($savePath='/uploads')
+    public function __construct($savePath = '/uploads')
     {
-        $this->path = $savePath;
+        $this->path = __DIR__ . '/../../../../' . $savePath;
     }
 
-    public function upload($urlToImg){
+    /**
+     * @param $urlToImg
+     * @return bool|int
+     * @throws \Exception
+     */
+    public function upload($urlToImg)
+    {
         $this->url = $urlToImg;
+        $this->updateImageType();
         $this->check();
+        return $this->saveImage();
+    }
 
-        $imgData = getimagesize($this->url);
-        if(!in_array($imgData['mime'],$this->format))
-            throw new \Exception('Недопустимый формат файла');
-
+    /**
+     * @return bool
+     * @throws \Exception
+     */
+    protected function saveImage(){
         $img = file_get_contents($this->url);
 
-        if($img === false)
+        if ($img === false) {
             throw new \Exception('Ошибка загрузки файла');
+        }
 
-
-
-        if(!file_exists($this->path))
+        if (!file_exists($this->path)) {
             mkdir($this->path);
+        }
 
-        file_put_contents($this->path.'/'.time().'_'.$imgData[2], $img);
+        $result = file_put_contents($this->path . '/' . time() . '.' . $this->imageType, $img);
+
+        return boolval($result);
     }
 
-    public function changeFormats(array $arr){
-        $this->format = $arr;
+    /**
+     * @return mixed
+     */
+    protected function updateImageType()
+    {
+        $imgData = getimagesize($this->url);
+        $this->imageType = explode('/', $imgData['mime'])[1];
+
+        return $this->imageType;
     }
 
-    protected function check(){
-        if(!is_string($this->url)){
+    /**
+     * @throws \Exception
+     */
+    protected function check()
+    {
+        if (!is_string($this->url)) {
             throw new \Exception('Адрес должен быть строкой');
         }
 
-        if(!filter_var($this->url, FILTER_VALIDATE_URL)){
+        if (!filter_var($this->url, FILTER_VALIDATE_URL)) {
             throw new \Exception('Строка не Url');
         }
 
-        if(!is_string($this->path)){
+        if (!is_string($this->path)) {
             throw new \Exception('Путь должен быть строкой');
+        }
+
+        if (!in_array('image/' . $this->imageType, $this->format)) {
+            throw new \Exception('Недопустимый формат файла');
         }
     }
 
